ff7d7a1f8675a767612e23ec164391f1b42d9cff
L.W.L <102495553@qq.com>
2019-04-25 21:50:33 +0800

    feat: pug template

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const Koa = require('koa')
+const ejs = require('ejs')
+const pug = require('pug')
 
 const app = new Koa()
 
-const { normal } = require('./tpl')
+const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
 
 app.use(async (ctx, next) => {
   ctx.type = 'text/html; charset=utf-8'
-  ctx.body = normal
+  ctx.body = pug.render(pugTpl, {
+    you: 'Vater pug',
+    me: 'Brolly pug'
+  })
+  // ctx.body = ejs.render(ejsTpl, {
+  //   you: 'Vater ejs',
+  //   me: 'Brolly ejs'
+  // })
 })
 
 app.listen(2266)
