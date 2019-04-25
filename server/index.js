6251c33b59b720bf055b2ff34c478be648e8a48f
L.W.L <102495553@qq.com>
2019-04-25 23:36:19 +0800

    feat: 模板引擎

--- server/index.js
+++ server/index.js
@@ -1,21 +1,35 @@
 const Koa = require('koa')
 const ejs = require('ejs')
 const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
 
 const app = new Koa()
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
 
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf-8'
-  ctx.body = pug.render(pugTpl, {
-    you: 'Vater pug',
-    me: 'Brolly pug'
+  await ctx.render('index', {
+    you: 'wenli',
+    me: 'Brolly'
   })
-  // ctx.body = ejs.render(ejsTpl, {
-  //   you: 'Vater ejs',
-  //   me: 'Brolly ejs'
-  // })
 })
 
 app.listen(2266)
+
+
+// app.use(async (ctx, next) => {
+//   ctx.type = 'text/html; charset=utf-8'
+//   ctx.body = pug.render(pugTpl, {
+//     you: 'Vater pug',
+//     me: 'Brolly pug'
+//   })
+//   // ctx.body = ejs.render(ejsTpl, {
+//   //   you: 'Vater ejs',
+//   //   me: 'Brolly ejs'
+//   // })
+// })
