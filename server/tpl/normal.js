fe4a0c62d7af0ae1d0ced7bf968241b06b9fe3c7
L.W.L <102495553@qq.com>
2019-04-25 18:16:50 +0800

    feat: 新增了 HTML页面

---
+++ server/tpl/normal.js
@@ -0,0 +1,25 @@
+module.exports = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Koa Server HTML</title>
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h1>Hi Vater</h1>
+            <p>This is Brolly</p>
+          </div>
+          <div class="col-md-4">
+            <p>测试静态 HTML 页面</p>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
+`
