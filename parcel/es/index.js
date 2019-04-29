ddb6824e20934751bdc3509fe0265c36c4b19edf
L.W.L <102495553@qq.com>
2019-04-29 14:00:09 +0800

    parcel

---
+++ parcel/es/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './app'
+
+class AppContainer extends React.Component {
+  state = {
+    name: 'Parcel 打包案例'
+  }
+  componentDidMount() {
+    setTimeout(() => this.setState({ name: 'parcel 打豆豆！' }), 2000)
+  }
+
+  render() {
+    return <App name={this.state.name} />
+  }
+}
+
+render(
+  <AppContainer />,
+  document.getElementById('app')
+)
---
+++ parcel/js/index.js
@@ -0,0 +1,6 @@
+$(function() {
+  function changeTitle() {
+    $('#app').html('Parcel 打包包')
+  }
+  setTimeout(changeTitle, 2000)
+})
